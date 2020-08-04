deb35288ce25de95a8c8ccbd6f5c10fc98157ea9
Gerardo Pérez <ISnape.jilc45gmail.com>
2020-08-03 20:59:44 -0500

    Perfiles

--- profiles/views.py
+++ profiles/views.py
@@ -8,7 +8,7 @@ from registration.models import Profile
 class ProfileListView(ListView):
     model = Profile
     template_name = 'profiles/profile_list.html'
-    paginate_by = 3
+    paginate_by = 12
 
 class ProfileDetailView(DetailView):
     model = Profile
