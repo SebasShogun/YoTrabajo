38e4d2c738ab557e94f0bb4c88ca404b7dcd15e3
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-08-07 19:11:44 -0500

    eliminación de ocupación

--- registration/forms.py
+++ registration/forms.py
@@ -19,11 +19,10 @@ class UserCreationFormWithEmail(UserCreationForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['avatar', 'bio', 'ocupation', 'link']
+        fields = ['avatar', 'bio', 'link']
         widgets = {
             'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
             'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
-            'ocupation': forms.TextInput(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Carpintero/Contratista'}),
             'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
         }
 
--- registration/models.py
+++ registration/models.py
@@ -12,9 +12,8 @@ def custom_upload_to(instance, filename):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
-    ocupation = models.TextField(blank=True)
     bio = models.TextField(null=True, blank=True)
-    link =models.URLField(max_length=200, null=True, blank=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
 
     class Meta:
         ordering = ['user__username']
@@ -23,4 +22,3 @@ class Profile(models.Model):
 def ensure_profile_exist(sender, instance, **kwargs):
     if kwargs.get('created', False):
         Profile.objects.get_or_create(user=instance)
-        # print("Se acaba de crear un usuario y su perfil enlazado")
